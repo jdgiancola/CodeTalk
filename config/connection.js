5324f59206e62e91e7673af914110beffe525864
Jo Giancola <josephdgiancola@gmail.com>
2023-11-30 11:42:07 -0600

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,27 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  const url = process.env.JAWSDB_URL;
+  const match = url.match(/mysql:\/\/(.*):(.*)@(.*):(\d*)\/(.*)/);
+
+  sequelize = new Sequelize(match[5], match[1], match[2], {
+    host: match[3],
+    port: match[4],
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: 'Amazon RDS'
+    },
+    // Additional options for production environment...
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -13,7 +30,14 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3306,
+      // Additional options for development environment...
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
     }
   );
 }
